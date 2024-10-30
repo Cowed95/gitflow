5a5ff83857a382c7c99b155d9e09fc2656c1f9bd
Juan NietoV <nieto.juan.8549@eam.edu.co>
2024-10-29 22:55:37 -0500

    ADD test Sumar y restar

--- src/main/test/com/eam/app/AppTest.java
+++ src/main/test/com/eam/app/AppTest.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.miempresa.miapp.App;
+
 class AppTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    void testSumar() {
+        double resultado = App.sumar(10, 5);
+        assertEquals(15, resultado, 0.001);
+    }
+
+    @Test
+    void testRestar() {
+        double resultado = App.restar(10, 5);
+        assertEquals(5, resultado, 0.001);
+    }
 
 }
