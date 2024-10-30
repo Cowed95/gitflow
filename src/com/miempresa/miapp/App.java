312abdae65ade89a468b973f7da463cde8e21373
Juan NietoV <nieto.juan.8549@eam.edu.co>
2024-10-29 22:18:31 -0500

    ADD multiplicar y divir

--- src/com/miempresa/miapp/App.java
+++ src/com/miempresa/miapp/App.java
@@ -10,4 +10,12 @@ public class App {
 		return a - b;
 	}
 
+	public static double multiplicar (double a, double b) {
+		return a * b;
+	}
+	
+	public static double dividir (double a, double b) {
+		return a / b;
+	}
+	
 }
